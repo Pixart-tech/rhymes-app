55197ceb14773beb1a433ddc60cd5ecb894b300f
surya4032 <chandraraotummala2@gmail.com>
2025-09-30 17:49:36 +0530

    server update

--- backend/server.py
+++ backend/server.py
@@ -750,6 +750,7 @@ async def get_rhyme_svg(rhyme_code: str):
 
     if RHYME_SVG_BASE_PATH is not None:
         # svg_path = RHYME_SVG_BASE_PATH / f"{rhyme_code}.svg"
+        
         svg_path= Path(r"\\pixartnas\home\RHYMES & STORIES\NEW\Rhymes\SVGs") / f"{rhyme_code}.svg"
         print(svg_path)
         try:
