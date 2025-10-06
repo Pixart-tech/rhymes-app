6af32f305e15a5a44521ab4b4a381fe847c46097
surya4032 <chandraraotummala2@gmail.com>
2025-10-06 15:12:08 +0530

    modified unc path

--- backend/app/config.py
+++ backend/app/config.py
@@ -46,7 +46,7 @@ DEFAULT_COVER_SVG_BASE_PATH = Path(
     r"\\pixartnas\home\Project ABC\Project ABC Cover\background\Sample"
 )
 NETWORK_COVER_SVG_BASE_PATH = Path(
-    r"\\pixartnas\home\Project ABC\Project ABC Cover\background\Sample"
+    r"pixartnas\home\Project ABC\Project ABC Cover\background\Sample\1 Theme\Theme 1 Test\SVGs"
 )
 PACKAGED_COVER_SVG_BASE_PATH = ROOT_DIR / "sample_cover_assets"
 
@@ -141,7 +141,7 @@ def build_cover_selection_paths(
     segments = [
         f"{theme_number} Theme",
         f"Theme {theme_number}",
-        f"Colour{colour_number})",
+        f"Colour{colour_number}"
     ]
 
     unc_path = parent_unc_path.joinpath(*segments)
